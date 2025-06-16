61f3a9a631a2cb9c83516ba47d97c26bda664c18
Mustafa Shaikh <smustfa147@gmail.com>
2025-06-16 23:16:49 +0800

    UnusualSpendsProcessor: Optimize Unusual spends processing

--- src/main/java/com/scw/ddd/unusual/spends/domain/service/UnusualSpendsProcessor.java
+++ src/main/java/com/scw/ddd/unusual/spends/domain/service/UnusualSpendsProcessor.java
@@ -1,11 +1,13 @@
 package com.scw.ddd.unusual.spends.domain.service;
 
 import com.scw.ddd.unusual.spends.config.UnusualSpendsConfig;
+import com.scw.ddd.unusual.spends.domain.valueobject.SpendingCategory;
 import com.scw.ddd.unusual.spends.dto.SpendingByCategoryAndAmount;
 import com.scw.ddd.unusual.spends.dto.UnusualSpend;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class UnusualSpendsProcessor {
 
@@ -21,26 +23,36 @@ public class UnusualSpendsProcessor {
 
         List<UnusualSpend> unusualSpends = new ArrayList<>();
 
-        for (SpendingByCategoryAndAmount currentMonthSpending: currentMonthSpendingByCategoryAndAmount) {
+        for (SpendingCategory category : SpendingCategory.values()) {
+            var currentMonthSpendingOptional = getSpendingByCategory(currentMonthSpendingByCategoryAndAmount, category);
 
-            for (SpendingByCategoryAndAmount lastMonthSpending: lastMonthSpendingByCategoryAndAmounts) {
+            var lastMonthSpendingOptional = getSpendingByCategory(lastMonthSpendingByCategoryAndAmounts, category);
 
-                if (currentMonthSpending.getCategory() == lastMonthSpending.getCategory()) {
+            if (currentMonthSpendingOptional.isPresent() && lastMonthSpendingOptional.isPresent()) {
+                var currentMonthSpending = currentMonthSpendingOptional.get();
+                var lastMonthSpending = lastMonthSpendingOptional.get();
 
-                    var percentage = getPercentage(
-                            currentMonthSpending.getAmount(),
-                            lastMonthSpending.getAmount()
-                    );
+                var percentage = getPercentage(
+                        currentMonthSpending.getAmount(),
+                        lastMonthSpending.getAmount()
+                );
 
-                    if (percentage >= unusualSpendsConfig.getPercentage()) {
-                        UnusualSpend unusualSpend = new UnusualSpend(currentMonthSpending.getCategory(), currentMonthSpending.getAmount());
-                        unusualSpends.add(unusualSpend);
-                    }
+                if (percentage >= unusualSpendsConfig.getPercentage()) {
+                    var unusualSpend = new UnusualSpend(currentMonthSpending.getCategory(), currentMonthSpending.getAmount());
+                    unusualSpends.add(unusualSpend);
                 }
+
             }
         }
 
         return unusualSpends;
+    }
+
+    private static Optional<SpendingByCategoryAndAmount> getSpendingByCategory(List<SpendingByCategoryAndAmount> spendingByCategoryAndAmount, SpendingCategory category) {
+        return spendingByCategoryAndAmount
+                .stream()
+                .filter(spendingByCategoryAndAmountForMonth -> category == spendingByCategoryAndAmountForMonth.getCategory())
+                .findFirst();
     }
 
 }
